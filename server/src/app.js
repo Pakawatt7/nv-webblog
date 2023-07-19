f9b83ba6040c8a9943d1aba48f6e7a86378ec4c7
Pakawatt7 <pakawats64@nu.ac.th>
2023-07-19 10:06:12 +0700

    update RESTFul

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,24 @@
-console.log('Hello nodejs server')
-console.log('Hello Milo')
-console.log('MiloO1tin')
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req, res) {
+    console.log('hello -' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
+})
+
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+app.get('/users', function (req, res) {
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
